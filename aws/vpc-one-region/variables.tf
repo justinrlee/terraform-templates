821ca60a24ad17eefbf977fec189178ca9baad95
Justin Lee <justin.r.lee@gmail.com>
2024-11-28 07:54:43 +0000

    Update vpc-one-region to default to no private resources

--- aws/vpc-one-region/variables.tf
+++ aws/vpc-one-region/variables.tf
@@ -3,6 +3,9 @@ variable "owner_name" {}
 variable "owner_contact" {}
 variable "date_updated" {}
 variable "myip" {}
+variable "enable_private" {
+    default = false
+}
 
 variable "public_type" {
     default = "t4g.medium"
