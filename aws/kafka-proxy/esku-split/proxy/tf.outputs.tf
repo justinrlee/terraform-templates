60f9ef69eed4df558bd52bf0f26359f30875ddc0
Justin Lee <justin.r.lee@gmail.com>
2025-01-16 01:56:28 +0000

    Update output to include bootstrap port

--- aws/kafka-proxy/esku-split/proxy/tf.outputs.tf
+++ aws/kafka-proxy/esku-split/proxy/tf.outputs.tf
@@ -1,5 +1,5 @@
 output "proxy_bootstrap" {
-    value = "${var.proxy_bootstrap}.${var.route_53_domain}"
+    value = "${var.proxy_bootstrap}.${var.route_53_domain}:9092"
 }
 
 output "bootstrap_server" {
