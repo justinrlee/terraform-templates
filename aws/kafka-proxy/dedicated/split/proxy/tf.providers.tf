ddbcbbc41b5b1eef0a34357ba690fe341c6cf94b
Justin Lee <justin.r.lee@gmail.com>
2025-01-20 03:48:41 +0000

    Update provenance

--- aws/kafka-proxy/dedicated/split/proxy/tf.providers.tf
+++ aws/kafka-proxy/dedicated/split/proxy/tf.providers.tf
@@ -13,7 +13,7 @@ provider "aws" {
     tags = {
       "tf_owner"         = var.owner_name
       "tf_owner_contact" = var.owner_contact
-      "tf_provenance"    = "github.com/justinrlee/terraform-templates/aws/kafka-proxy/split/proxy"
+      "tf_provenance"    = "github.com/justinrlee/terraform-templates/aws/kafka-proxy/dedicated/split/proxy"
       "tf_workspace"     = terraform.workspace
       "tf_last_modified" = var.date_updated
       "Owner"            = var.owner_name
